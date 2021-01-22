caebaabd9e13cc3b849c8422eee9acd67e504c33
matthias1005 <74554723+matthias1005@users.noreply.github.com>
2021-01-22 15:53:32 +0100

    Version 2.0
    added Fuel Gauge

--- speedo.lua
+++ speedo.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(10)
+        Citizen.Wait(0)
         if IsPedInAnyVehicle(GetPlayerPed(-1), true) then
             SendNUIMessage({type = 'ui' , status = true})
             currentspeed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false)) * 3.6
@@ -8,7 +8,10 @@ Citizen.CreateThread(function()
             speed = currentspeed .. "km/h"
             halfspeed = currentspeed/3
             halfspeed = halfspeed .. "%"
+            fuel = GetVehicleFuelLevel(GetVehiclePedIsIn(GetPlayerPed(-1)))
+            fuelwidth = fuel .."%"
             SendNUIMessage({type = 'updatekmh', speed = speed, speedwidth = halfspeed})
+            SendNUIMessage({type = 'updatefuel', fuel = fuelwidth})
         else
             SendNUIMessage({type = 'ui' , status = false})
         end
